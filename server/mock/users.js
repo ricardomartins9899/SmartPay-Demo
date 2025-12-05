a1cb882c4da3dcd107a4d43c0d2c0e819b54e951
Guy First <guyfirst19@gmail.com>
2025-12-05 12:06:05 +0200

    Update users.js
    changed the magic number 10 to a variable, changed to 12 for stronger hashing

--- server/mock/users.js
+++ server/mock/users.js
@@ -1,12 +1,12 @@
  
 const bcrypt = require('bcryptjs');
 const axios = require("axios");
-
+const SALT = 12;
 const users = []; // { id, email, password_hash, name?, role? }
 let nextId = 1;
 
 async function createUser(email, password, name = null, role = null) {
-  const password_hash = await bcrypt.hash(password, 10);
+  const password_hash = await bcrypt.hash(password, SALT);
   const user = { id: nextId++, email, password_hash, name, role };
   users.push(user);
   return { id: user.id, email: user.email, name: user.name, role: user.role };
